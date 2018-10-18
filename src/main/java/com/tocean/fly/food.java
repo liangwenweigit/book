f108825146c42b7ace2e31fbaff5c2afa5d18020
root2 <137818850_2@qq.com>
2018-10-18 17:12:45 +0800

    第一次提交（日志）

---
+++ src/main/java/com/tocean/fly/food.java
@@ -0,0 +1,8 @@
+package com.tocean.fly;
+
+public class food {
+	public static void main(String[] args) {
+		System.out.println("初始化");
+	}
+
+}
---
+++ src/test/java/com/tocean/fly/testFood.java
@@ -0,0 +1,8 @@
+package com.tocean.fly;
+
+public class testFood {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+}
